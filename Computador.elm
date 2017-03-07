15ce38258f468651eb653a7b6c5430a474449a59
GiuRz <curso.livre@belasartes.edu.br>
2017-03-07 20:24:06 -0300

    ch
    h

--- Computador.elm
+++ Computador.elm
@@ -1,5 +1,16 @@
 module main exposing (..)
 
+import Html exposing ( div, button, text)
+--modelo
+
 modelo =
     0
-    
+
+
+ -- vista
+
+main = div[]
+  [ button [] [text "-"]
+  , text (toString modelo)
+  , button [] [text "+"]
+  ]
